315e2516b9095f895e2497f7aa0dd4c582a94db1
hello <hello@hellos-MacBook-Pro-2.local>
2025-07-27 09:12:21 +0700

    Update Error 404

--- blog/settings.py
+++ blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v40*=5qv%)q^=sb-kpav+&uvc3!^%8jx%9nu%0_g@%$w^u^^$w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['betatips.com', 'wwww.betatips.com', '127.0.0.1']
+ALLOWED_HOSTS = ['betatips.com', 'wwww.betatips.com', '127.0.0.1', '*']
 CSRF_TRUSTED_ORIGINS = ['https://betatips.com']
 
 
